9c76cc58502deb53c5c9c9936a992a6cff1ad4e6
Huukha182624 <khanhse182624@fpt.edu.vn>
2025-05-30 23:49:17 +0700

    Update App.jsx
    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-// App.jsx
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import MainContent from './components/MainContent'; // Trang chủ
+import About from './pages/About'; // Giới thiệu
 
 function App() {
     return (
@@ -9,6 +10,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<MainContent />} />
+                    <Route path="gioi-thieu" element={<About />} />
+                    {/* Bạn có thể thêm các route khác ở đây, ví dụ:
+          <Route path="chuyen-mon" element={<ChuyenMon />} />
+          */}
                 </Route>
             </Routes>
         </Router>
