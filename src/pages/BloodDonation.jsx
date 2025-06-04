7fbb1bc8ae799314684915135c23ce5de9b72a08
Huukha182624 <khanhse182624@fpt.edu.vn>
2025-06-04 11:19:02 +0700

    Update BloodDonation.jsx

--- src/pages/BloodDonation.jsx
+++ src/pages/BloodDonation.jsx
@@ -1,13 +1,13 @@
 // BloodDonation.jsx
 import React from "react";
 import BloodDonationStandards from "../components/BloodDonationStandards";
-
-
-import Tips from "../components/Tips"; // bạn có thể chia phần lời khuyên ra file riêng
+import Tips from "../components/Tips";
+import DonorBenefits from "../components/DonorBenefit";
 
 const BloodDonation = () => {
     return (
         <>
+            <DonorBenefits />
             <BloodDonationStandards />
             <Tips />
         </>
