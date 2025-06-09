28c0eabde560fb04d530743e9366793bfa614537
Huukha182624 <khanhse182624@fpt.edu.vn>
2025-06-09 22:07:55 +0700

    Update BannerSlider.jsx

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -17,7 +17,7 @@ const BannerSlider = () => {
         infinite: true,
         speed: 1000,
         autoplay: true,
-        autoplaySpeed: 7000,
+        autoplaySpeed: 5000,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
